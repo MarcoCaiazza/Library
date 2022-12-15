12fd5c3c298b430bfea3f750de9b7877aa5d6364
Marco <Marco@example.com>
2022-12-15 21:19:47 +0100

    creato tasti plus e cancel

--- Oggetti e Costruttori di oggetti.js
+++ Oggetti e Costruttori di oggetti.js
@@ -1,8 +1,19 @@
 const btnBook = document.querySelector("#btn");
+const module = document.querySelector(".module");
+const cancel = document.querySelector(".cancel");
 
-const createTable = () => {};
-
+const createTable = () => {
+  const module = document.querySelector(".module");
+  if (module.style.display === "none") {
+    module.style.display = "flex";
+  }
+};
 btnBook.addEventListener("click", createTable);
+module.style.display = "none";
+const cancelForm = () => {
+  module.style.display = "none";
+};
+cancel.addEventListener("click", cancelForm);
 
 let myLibrary = [];
 
