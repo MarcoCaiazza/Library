903a8fe97478c75a76c129fb826c279c4383c529
Marco <Marco@example.com>
2023-01-19 22:15:41 +0100

    aggiunto tasto eliminazione libro

--- Oggetti e Costruttori di oggetti.js
+++ Oggetti e Costruttori di oggetti.js
@@ -44,30 +44,54 @@ const addBookToLibrary = () => {
   containerLibrary.innerHTML = "";
   for (let i = 0; i < myLibrary.length; i++) {
     if (myLibrary[i] !== lastBook) {
-      displayBook(myLibrary[i]);
+      displayBook(myLibrary[i], i);
     }
   }
 };
 
-const displayBook = (book) => {
+const displayBook = (book, i) => {
   let myBook = document.createElement("div");
+  myBook.setAttribute("data-id", i);
+  console.log(myBook.setAttribute("data-id", i));
   myBook.className = "myBook";
 
   let bookTitle = document.createElement("p");
-  bookTitle.textContent = `${book.title}`;
+  bookTitle.textContent = `Title: ${book.title}`;
   myBook.appendChild(bookTitle);
 
   let bookAuthor = document.createElement("p");
-  bookAuthor.textContent = `${book.author}`;
+  bookAuthor.textContent = `Author: ${book.author}`;
   myBook.appendChild(bookAuthor);
 
   let bookPages = document.createElement("p");
-  bookPages.textContent = `${book.pages}`;
+  bookPages.textContent = `Pages: ${book.pages}`;
   myBook.appendChild(bookPages);
 
   let bookRead = document.createElement("p");
-  bookRead.textContent = `${book.read}`;
+  bookRead.textContent = `Read: ${book.read}`;
+  bookRead.setAttribute("data-id", book.read);
   myBook.appendChild(bookRead);
 
+  let divImages = document.createElement("div");
+  divImages.className = "divImages";
+
+  trashImg = document.createElement("img");
+  trashImg.src = "images/icons8-elimina.svg";
+
+  glassesImg = document.createElement("img");
+  glassesImg.src = "images/icons8-binocolo.svg";
+
+  divImages.appendChild(glassesImg);
+  divImages.appendChild(trashImg);
+
+  myBook.appendChild(divImages);
+
   containerLibrary.appendChild(myBook);
+
+  trashImg.addEventListener("click", function () {
+    let index = myBook.getAttribute("data-id");
+    myLibrary.splice(index, 1);
+    console.log(myLibrary);
+    addBookToLibrary();
+  });
 };
